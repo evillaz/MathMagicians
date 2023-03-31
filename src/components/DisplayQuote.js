4dd03051956307014327055c1d61b7ea51403c88
evillaz <evillaz123@gmail.com>
2023-03-31 06:20:48 -0500

    display quotes using events

---
+++ src/components/DisplayQuote.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const DisplayQuote = () => {
+  const [data, setData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const url = 'https://api.api-ninjas.com/v1/quotes?category=leadership';
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axios.get(url, {
+          headers: {
+            'X-Api-Key': 'k7shCDZxzekW1jb0EMX/Fg==FsmufvqiSIUwWL1Q',
+          },
+          mode: 'cors',
+        });
+        const result = res.data[0];
+        setData(result);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchQuote();
+  }, [setData, setIsLoading]);
+  if (hasError) return <div>Something went wrong!</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="quote">
+      {data.quote}
+      <span className="author">
+        <br />
+        &quot;
+        {data.author}
+        &quot;
+      </span>
+    </div>
+  );
+};
+
+export default DisplayQuote;
